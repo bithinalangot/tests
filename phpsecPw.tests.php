eed42f564d9fb5c461d6c8407dcd17875f505423
Bithin A <bithin2007@gmail.com>
2011-12-18 15:19:38 +0530

    Have written the code for testing the class phpsecPw.

--- phpsecPw.tests.php
+++ phpsecPw.tests.php
@@ -1,1 +1,25 @@
 <?php
+
+/**
+*
+  phpSecPw tests.
+
+  @author Bithin A <bithin2007@gmail.com>
+  @copyright Copyright (c) Audun Larsen, 2011
+  @link https://github.com/phpsec/tests
+  @license http://opensource.org/licenses/mit-license.php The MIT License
+  @package phpSec-tests
+
+*/
+
+require_once 'phpSec/phpsec/phpsec.pw.php';
+
+$tf -> test("phpsecPW: hash()",function($tf){
+
+    $password = 'yourpassword';
+    $dbpassword = phpsecPw::hash('yourpassword');
+    $tf->assert(phpsecPw::check($password,$dbpassword));
+
+});
+
+?>
